3d8c2eb01d1d3417f9077796ef0a94096ec8b9d5
Isaac4321567 <Isaac4321567@gmail.com>
2020-02-27 21:28:34 +0800

    implementing react. This will be the last commit that keeps the files for not react.

---
+++ todo_list/app/javascript/components/Album.js
@@ -0,0 +1,312 @@
+import React from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+
+import Button from '@material-ui/core/Button';
+
+
+import Card from '@material-ui/core/Card';
+
+import CardActions from '@material-ui/core/CardActions';
+
+import CardContent from '@material-ui/core/CardContent';
+
+import CardMedia from '@material-ui/core/CardMedia';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Grid from '@material-ui/core/Grid';
+
+import Toolbar from '@material-ui/core/Toolbar';
+
+import Typography from '@material-ui/core/Typography';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import Container from '@material-ui/core/Container';
+
+import Link from '@material-ui/core/Link';
+
+import Chip from '@material-ui/core/Chip';
+
+function Copyright() {
+
+  return (
+
+    <Typography variant="body2" color="textSecondary" align="center">
+
+      {'Copyright © '}
+
+      <Link color="inherit" href="https://material-ui.com/">
+
+        Your Website
+
+      </Link>{' '}
+
+      {new Date().getFullYear()}
+
+      {'.'}
+
+    </Typography>
+
+  );
+
+}
+
+
+
+const useStyles = makeStyles(theme => ({
+
+  icon: {
+
+    marginRight: theme.spacing(2),
+
+  },
+
+  heroContent: {
+
+    backgroundColor: theme.palette.background.paper,
+
+    padding: theme.spacing(8, 0, 6),
+
+  },
+
+  heroButtons: {
+
+    marginTop: theme.spacing(4),
+
+  },
+
+  cardGrid: {
+
+    paddingTop: theme.spacing(8),
+
+    paddingBottom: theme.spacing(8),
+
+  },
+
+  card: {
+
+    height: '100%',
+
+    display: 'flex',
+
+    flexDirection: 'column',
+
+  },
+
+  cardMedia: {
+
+    paddingTop: '10%', // 16:9
+    backgroundColor: theme.palette.warning.light,
+
+  },
+
+  cardContent: {
+
+    flexGrow: 1,
+
+  },
+
+  footer: {
+
+    backgroundColor: theme.palette.background.paper,
+
+    padding: theme.spacing(6),
+
+  },
+
+  chipcontainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(0.5),
+    },
+  },
+
+}));
+
+
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+
+export default function Album() {
+
+  const classes = useStyles();
+
+  const handleDelete = () => {};
+
+  return (
+
+    <React.Fragment>
+
+      <CssBaseline />
+
+      <AppBar position="relative">
+
+        <Toolbar>
+
+
+          <Typography variant="h6" color="inherit" noWrap>
+
+            CVWO 2020
+
+          </Typography>
+
+        </Toolbar>
+
+      </AppBar>
+
+      <main>
+
+        {/* Hero unit */}
+
+        <div className={classes.heroContent}>
+
+          <Container maxWidth="sm">
+
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+
+              Todo List
+
+            </Typography>
+
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+
+              A simple To-do app developed using Material-UI Album react component template
+
+            </Typography>
+
+            <div className={classes.heroButtons}>
+
+              <Grid container spacing={2} justify="center">
+
+                <Grid item>
+
+                  <Button variant="contained" color="primary">
+
+                    Add a Todo
+
+                  </Button>
+
+                </Grid>
+
+                <Grid item>
+
+                  <Button variant="outlined" color="primary">
+
+                    Secondary action
+
+                  </Button>
+
+                </Grid>
+
+              </Grid>
+
+            </div>
+
+          </Container>
+
+        </div>
+
+        <Container className={classes.cardGrid} maxWidth="md">
+
+          {/* End hero unit */}
+
+          <Grid container spacing={4}>
+
+            {cards.map(card => (
+
+              <Grid item key={card} xs={12} sm={6} md={4}>
+
+                <Card className={classes.card}>
+
+                  <CardMedia
+
+                    className={classes.cardMedia}
+
+
+                  />
+
+                  <CardContent className={classes.cardContent}>
+
+                    <Typography gutterBottom variant="h5" component="h2">
+
+                      Heading
+
+                    </Typography>
+
+                    <Typography>
+
+                      This is a media card. You can use this section to describe the content.
+
+                    </Typography>
+
+                  </CardContent>
+
+                  <CardActions>
+
+                    <Button size="small" color="primary">
+
+                      View
+
+                    </Button>
+
+                    <Button size="small" color="primary">
+
+                      Edit
+
+                    </Button>
+
+                  </CardActions>
+
+		  <div className={classes.chipcontainer}>
+		  <Chip
+		    label="Important"
+		    color="primary"
+		    variant="outlined"
+		  />
+		  </div>
+
+                </Card>
+
+              </Grid>
+
+            ))}
+
+          </Grid>
+
+        </Container>
+
+      </main>
+
+      {/* Footer */}
+
+      <footer className={classes.footer}>
+
+        <Typography variant="h6" align="center" gutterBottom>
+
+          Footer
+
+        </Typography>
+
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+
+          Something here to give the footer a purpose!
+
+        </Typography>
+
+        <Copyright />
+
+      </footer>
+
+      {/* End footer */}
+
+    </React.Fragment>
+
+  );
+
+}
--- todo_list/app/javascript/packs/application.js
+++ todo_list/app/javascript/packs/application.js
@@ -12,3 +12,7 @@
 //= require_tree.
 
 //= require_new
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
